eba97235c9ea90d5a42291ebc81411b9a026d188
R-Liebert <R_Liebert@outlook.com>
2023-01-04 09:51:44 +0100

    Update run_sigopt.py

--- run_sigopt.py
+++ run_sigopt.py
@@ -1,5 +1,7 @@
 import os
 
+# File for running all experiments on the UNIX server.
+
 os.system("python ~/bhome/DQN_MountainCar_sigopt.py")
 os.system("python ~/bhome/Double-DQN_MountainCar_sigopt.py")
 os.system("python ~/bhome/DQN_Acrobot_sigopt.py")
